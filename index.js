077be8764dc03d209229020930a207854c6eafda
mayowa Bernard <bernardmayowa17@gmail.com>
2024-04-03 00:30:33 +0100

    Completed database connection setup

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import dbConnect from "./config/dbConnect.js";
+
+dotenv.config()
+
+
 
 const app = express()
 const port = process.env.PORT || 3001;
+
+
+dbConnect();
 
 app.use("/", (req, res) => {
     res.send(" Maxifit server is running")
